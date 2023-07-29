07a14c760721e8b7e2e12c6b620826fa3fbd5904
Hudson <hudsonbeandev@gmail.com>
2023-07-29 16:15:54 -0500

    Example of bem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import "./styles/App.css";
 import { Person } from "./components/Person";
 
 const App = () => {
-  return <Person firstName="John" />;
+  return (
+    <>
+      <Person firstName="John" />
+      {/* Example of bem */}
+      <div className="test test--dark">
+        <div className="test__prototype">Hello</div>
+      </div>
+    </>
+  );
 };
 
 export default App;
