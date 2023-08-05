8a0d39d4ef04792a8ed1f5fee67927ade01f939b
Hudson <hudsonbeandev@gmail.com>
2023-08-05 01:19:20 -0500

    Created server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(process.env.PORT);
+
+//Models
+const dev = require("./models/dev");
+
+//Routing
+//dev
+app.get("/dev", async (req, res) => {
+  //Get data
+  const data = await dev.find();
+  //Response
+  res.json(data);
+});
