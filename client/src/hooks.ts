84f663e8c679fc641157c3592e1d8ffdab829c3e
Hudson <hudsonbeandev@gmail.com>
2023-08-08 15:53:02 -0500

    delete client

--- client/src/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import { RootState, AppDispatch } from "./store";
-import { Root } from "react-dom/client";
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- client/src/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import dataReducer from "./features/slices/dataSlice";
-
-export const store = configureStore({
-  reducer: { data: dataReducer },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
