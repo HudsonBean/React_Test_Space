a1f2224fe23a87a79690fb6ee01bdcf4f2030eb9
Hudson <hudsonbeandev@gmail.com>
2023-08-09 11:04:46 -0500

    YES I DID IT!!!

--- client/src/components/Data.tsx
+++ client/src/components/Data.tsx
@@ -4,6 +4,11 @@ import { useGetAllProductsQuery } from "../features/apiSlice";
 export const Data = () => {
   const { data, error, isLoading } = useGetAllProductsQuery(undefined);
   console.log(data, error, isLoading);
-
-  return <div></div>;
+  return (
+    <div>
+      {data?.products.map((i) => (
+        <p key={i.id}>{i.title}</p>
+      ))}
+    </div>
+  );
 };
