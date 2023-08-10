ed13e179d971427d29318dee332bcc8991816198
Hudson <hudsonbeandev@gmail.com>
2023-08-10 09:00:43 -0500

    Added routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { Home } from "./pages/Home";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Home />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
